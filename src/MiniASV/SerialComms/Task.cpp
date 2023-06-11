4bcda3e1ec0f5ca8190610c6d4670954f08a54b0
elcapitanu <bernardoacgabriel@gmail.com>
2023-06-11 16:30:03 +0100

    IMU Fixed and Angle Offset

--- src/MiniASV/SerialComms/Task.cpp
+++ src/MiniASV/SerialComms/Task.cpp
@@ -233,8 +233,7 @@ namespace MiniASV
         m_eulerAngles.setTimeStamp(m_tstamp);
         m_eulerAngles.phi = (m_driver->m_miniASVData.roll) / Math::c_degrees_per_radian;
         m_eulerAngles.theta = (m_driver->m_miniASVData.pitch) / Math::c_degrees_per_radian;
-        //m_eulerAngles.psi = -(m_driver->m_miniASVData.yaw) / Math::c_degrees_per_radian) + Angles::radians(m_args.angle_offset); // for some unkown reason I had to reserve the angle, in order to work
-        m_eulerAngles.psi = Angles::normalizeRadian(-(m_driver->m_miniASVData.yaw)); // for some unkown reason I had to reserve the angle, in order to work
+        m_eulerAngles.psi = Math::trimValue(-(m_driver->m_miniASVData.yaw) / Math::c_degrees_per_radian + Angles::radians(m_args.angle_offset), - Math::c_pi, Math::c_pi); // for some unkown reason I had to reserve the angle, in order to work
         m_eulerAngles.psi_magnetic = m_eulerAngles.psi;
         //war("YAW: %f", m_eulerAngles.psi);
         dispatch(m_eulerAngles, DF_KEEP_TIME);
