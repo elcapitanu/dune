9dc940f095c7baa7dd1a8c46ff4be2fc4853ec5d
Bernardo Gabriel <bernardoacgabriel@gmail.com>
2023-08-29 16:48:31 +0100

    Devices/Backbone: implemented driver to use SerialPort and added a consume for RemoteActions.

--- src/Devices/Backbone/Task.cpp
+++ src/Devices/Backbone/Task.cpp
@@ -24,11 +24,19 @@
 // https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
 // http://ec.europa.eu/idabc/eupl.html.                                     *
 //***************************************************************************
-// Author: Bernardo Gabriel                                                 *
+// Author: Ricardo Martins (GPS driver)                                     *
+// Author: Luis Venancio (BasicDeviceDriver compatibility)                  *
+// Author: Bernardo Gabriel (adaptation to Backbone)                        *
 //***************************************************************************
 
+// ISO C++ 98 headers.
+#include <algorithm>
+#include <cstddef>
+#include <cstring>
 // DUNE headers.
 #include <DUNE/DUNE.hpp>
+// Local headers.
+#include "Reader.hpp"
 
 namespace Devices
 {
@@ -40,14 +48,60 @@ namespace Devices
   {
     using DUNE_NAMESPACES;
 
-    struct Task: public DUNE::Tasks::Task
+    struct Arguments
     {
+      //! IO device (URI).
+      std::string io_dev;
+      //! Input timeout in seconds.
+      float inp_tout;
+    };
+
+    struct Task : public Hardware::BasicDeviceDriver
+    {
+      //! Serial port handle.
+      IO::Handle *m_handle;
+      //! Task arguments.
+      Arguments m_args;
+      //! Input watchdog.
+      Time::Counter<float> m_wdog;
+      //! Reader thread.
+      Reader *m_reader;
+      //! Buffer forEntityState
+      char m_bufer_entity[64];
+
       //! Constructor.
       //! @param[in] name task name.
       //! @param[in] ctx context.
-      Task(const std::string& name, Tasks::Context& ctx):
-        DUNE::Tasks::Task(name, ctx)
+      Task(const std::string &name, Tasks::Context &ctx) : Hardware::BasicDeviceDriver(name, ctx),
+                                                           m_handle(NULL),
+                                                           m_reader(NULL)
       {
+        // Define configuration parameters.
+        paramActive(Tasks::Parameter::SCOPE_GLOBAL,
+                    Tasks::Parameter::VISIBILITY_DEVELOPER,
+                    true);
+
+        param("IO Port - Device", m_args.io_dev)
+            .defaultValue("")
+            .description("IO device URI in the form \"uart://DEVICE:BAUD\"");
+
+        param("Input Timeout", m_args.inp_tout)
+            .units(Units::Second)
+            .defaultValue("4.0")
+            .minimumValue("0.0")
+            .description("Input timeout");
+
+        // Use wait for messages
+        setWaitForMessages(1.0);
+
+        bind<IMC::DevDataText>(this);
+        bind<IMC::IoEvent>(this);
+        bind<IMC::RemoteActions>(this);
+      }
+
+      ~Task() override
+      {
+        onDisconnect();
       }
 
       //! Update internal state with new parameter values.
@@ -56,44 +110,111 @@ namespace Devices
       {
       }
 
-      //! Reserve entity identifiers.
-      void
-      onEntityReservation(void)
+      //! Try to connect to the device.
+      //! @return true if connection was established, false otherwise.
+      bool
+      onConnect() override
       {
-      }
-
-      //! Resolve entity names.
-      void
-      onEntityResolution(void)
-      {
-      }
-
-      //! Acquire resources.
-      void
-      onResourceAcquisition(void)
-      {
-      }
-
-      //! Initialize resources.
-      void
-      onResourceInitialization(void)
-      {
-      }
-
-      //! Release resources.
-      void
-      onResourceRelease(void)
-      {
-      }
-
-      //! Main loop.
-      void
-      onMain(void)
-      {
-        while (!stopping())
+        try
         {
-          waitForMessages(1.0);
+          m_handle = openDeviceHandle(m_args.io_dev);
+          m_reader = new Reader(this, m_handle);
+          m_reader->start();
+          setEntityState(IMC::EntityState::ESTA_BOOT, Status::CODE_ACTIVATING);
+          return true;
         }
+        catch (...)
+        {
+          throw RestartNeeded(DTR(Status::getString(CODE_COM_ERROR)), 5);
+        }
+
+        return false;
+      }
+
+      //! Disconnect from device.
+      void
+      onDisconnect() override
+      {
+        if (m_reader != NULL)
+        {
+          m_reader->stopAndJoin();
+          Memory::clear(m_reader);
+        }
+
+        Memory::clear(m_handle);
+      }
+
+      //! Initialize device.
+      void
+      onInitializeDevice() override
+      {
+        setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
+
+        // m_wdog.setTop(m_args.inp_tout);
+      }
+
+      void
+      consume(const IMC::DevDataText *msg)
+      {
+        if (msg->getDestination() != getSystemId())
+          return;
+
+        if (msg->getDestinationEntity() != getEntityId())
+          return;
+
+        trace("%s", sanitize(msg->value).c_str());
+      }
+
+      void
+      consume(const IMC::IoEvent *msg)
+      {
+        if (msg->getDestination() != getSystemId())
+          return;
+
+        if (msg->getDestinationEntity() != getEntityId())
+          return;
+
+        if (msg->type == IMC::IoEvent::IOV_TYPE_INPUT_ERROR)
+          throw RestartNeeded(msg->error, 5);
+      }
+
+      void
+      consume(const IMC::RemoteActions *msg)
+      {
+        if (msg->getDestination() != getSystemId())
+          return;
+
+        Utils::TupleList tuples(msg->actions);
+        int motor = tuples.get("Motor", 0);
+        int piston = tuples.get("Piston", 0);
+        int rudder = tuples.get("Rudder", 0);
+
+        trace("motor: %d | piston: %d | rudder: %d", motor, piston, rudder);
+
+        char m_motor[64], m_piston[64], m_rudder[64];
+
+        std::sprintf(m_motor, "MOTOR,%d,\r\n", motor);
+        std::sprintf(m_piston, "PISTON,%d,\r\n", piston);
+        std::sprintf(m_rudder, "RUDDER,%d,\r\n", rudder);
+
+        m_handle->writeString(m_motor);
+        m_handle->writeString(m_piston);
+        m_handle->writeString(m_rudder);
+      }
+
+      //! Check for input timeout.
+      //! Data is read in the DevDataText consume.
+      //! @return true.
+      bool
+      onReadData() override
+      {
+        // if (m_wdog.overflow())
+        // {
+        //   setEntityState(IMC::EntityState::ESTA_ERROR, Status::CODE_COM_ERROR);
+        //   throw RestartNeeded(DTR(Status::getString(CODE_COM_ERROR)), 5);
+        // }
+
+        return true;
       }
     };
   }
