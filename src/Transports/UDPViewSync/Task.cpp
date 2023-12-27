a02884555826aeec1d091b74c541e6be3a870dc3
elcapitanu <bernardoacgabriel@gmail.com>
2023-12-27 12:37:00 +0000

    Transports/UDPViewSync: using only two group members.

--- src/Transports/UDPViewSync/Task.cpp
+++ src/Transports/UDPViewSync/Task.cpp
@@ -47,7 +47,7 @@ namespace Transports
     using DUNE_NAMESPACES;
 
     //! Total number of group members.
-    static const unsigned c_total_members = 4;
+    static const unsigned c_total_members = 2;
 
     //! Task arguments.
     struct Arguments
