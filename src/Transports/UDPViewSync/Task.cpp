840ca52e4b3b69fa065870cc65f6e2873241778a
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2024-01-10 17:58:53 +0000

    Transports/UDPViewSync: removed test time and added periodic feedback.

--- src/Transports/UDPViewSync/Task.cpp
+++ src/Transports/UDPViewSync/Task.cpp
@@ -350,16 +350,23 @@ namespace Transports
       void
       onMain(void)
       {
-        while(!String::endsWith(Format::getTimeSafe(), "0"));
+        // while(!String::endsWith(Format::getTimeSafe(), "0"));
 
         m_state = ACTIVE;
         
-        Time::Counter<float> test(5.0);
+        // Time::Counter<float> test(20.0);
+        Time::Counter<float> p(1.0);
         
         while (!stopping())
         {
-          if (test.overflow())
-            m_state = IDLE;
+          // if (test.overflow())
+            // m_state = IDLE;
+
+          if (p.overflow())
+          {
+            inf("%u | %u | %u", m_time_vector[0], m_time_vector[1], m_time_vector[2]);
+            p.reset();
+          }
 
           waitForMessages(0.1);
 
